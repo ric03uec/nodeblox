f85aa8926b7fe852495e0f4b0ace59bbc20c90e7
devashish <devashish.86@gmail.com>
2012-02-05 12:08:28 +0530

    layout changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 var util = require('util');
 var Logger = require('devnull');
-var logger = new Logger();
+var logger = new Logger({namespacing : 0});
 var User  = require('../schemas/User');
 /*
  * GET home page.
@@ -12,7 +12,6 @@ module.exports = function(app){
     * Map the URL '/' to the callback
     */
   app.get('/', function(req, res){
-    util.log('Serving request for url [GET]' + req.route.path)
     logger.log('Serving request for url [GET]' + req.route.path)
     
     res.render('index', { title: 'nodeBlox' })
@@ -22,7 +21,7 @@ module.exports = function(app){
     * Map the URL '/login' to the callback
     */
   app.post('/login', function(req, res){
-    util.log('Serving request for url [POST] ' + req.route.path);
+    logger.log('Serving request for url [POST] ' + req.route.path);
     var username = req.body.User;
     var password = req.body.Password;
   

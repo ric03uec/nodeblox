e4994ec0b5d0206d16ca2075ea4ffc0a69635c8b
devashish <devashish.86@gmail.com>
2012-04-22 10:20:26 +0530

    added 'wysihtml5' text editor in admin page

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 function loadScript(url, callback){
-  var head = document.getElementsByTagName('head')[0];
+  var footer = document.getElementById('footer');
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = url;
@@ -7,11 +7,25 @@ function loadScript(url, callback){
   script.onreadystatechange = callback;
   script.onload = callback;
 
-  head.appendChild(script);
+  footer.appendChild(script);
+}
+
+function initIndexPage(){
+  console.log('initializing');
+  var editor = $('#postArea').wysihtml5({
+      "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+      "emphasis": true, //Italics, bold, etc. Default true
+      "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+      "html": false, //Button which allows you to edit the generated HTML. Default false
+      "link": true, //Button to insert a link. Default true
+      "image": true //Button to insert an image. Default true;
+    });
 }
 
 $(document).ready(function(response){
   loadScript('js/login.js', function(){
     console.log('Login.js Loaded');  
   });
+  
+  initIndexPage();
 });

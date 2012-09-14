37755200db4fed45f0ffa1e5d697cfee877126b1
devashish <devashish.86@gmail.com>
2012-09-14 08:22:02 +0530

    testing requirejs functionality
    Conflicts:
    
    	public/js/index.js
    	views/index.jade

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,33 @@
+//this is the main file which will be instantiated upon js load in browser
+//require the basic libraries here
+//require('jquery');
+
+//define a module here that will initialize main page widgets
+define(['jquery', 'knockout', 'bootstrap-wysihtml5'], function($, KO){
+  var Index = {};
+  var abc = $('#signupMessage').val();
+  console.log(abc);
+  console.log(KO);
+  console.log($('#postContent1'));
+  $('#postContent1').wysihtml5({
+    "font-styles": true, 
+    "emphasis": true, 
+    "lists": true, 
+    "html": false, 
+    "link": true,   
+    "image": true 
+    });
+
+
+
+  Index.initialize = function(){
+    console.log('indexjs initialized....');
+  };
+  return Index;
+});
+
+
+/*
 var Main = {};
 
 Main.loadScript = function(url){
@@ -13,4 +43,5 @@ Main.loadScript = function(url){
 $(document).ready(function(response){
   Main.loadScript('js/login.js');
   Main.loadScript('js/signup.js');
-});
+  Main.initUrls();
+});*/

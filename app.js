588d9a56114203f3e9cc35d0dbdd37be7ebe78e3
devashish <devashish.86@gmail.com>
2013-01-30 20:31:55 +0530

    adding redis store to production configuration

--- app.js
+++ app.js
@@ -9,20 +9,31 @@ var config = require('./config');
 var Logger = require('devnull');
 var logger = new Logger({namespacing : 0});
 var mongoose = require('mongoose');
+var RedisStore = require('connect-redis')(express);
+var redis = require('redis');
 var path = require('path');
 var http = require('http');
+
 
 var app  = express();
 mongoose.connect('mongodb://localhost/testdb');
 
 /**
-  * Application Configuration
+  * Application Global Configuration
   */
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.enable('jsonp callback');
   app.set('view engine', 'jade');
   app.set('view options', {layout : false});
+  app.use(express.static(path.join(__dirname + '/public')));
+});
+
+/**
+  * Application environment(s) Configuration
+  */
+app.configure('development', function(){
+
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({
@@ -36,15 +47,6 @@ app.configure(function(){
   });
 
   app.use(app.router);
-  app.use(express.static(path.join(__dirname + '/public')));
-});
-
-
-
-/**
-  * Application environment(s) Configuration
-  */
-app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
@@ -54,22 +56,34 @@ app.configure('stage', function(){
 
 app.configure('production', function(){
   //use redis store witht the following configuration
-  var redisConfig = {
-    'host' : '127.0.0.1',
-    'port' : 6397
-  };
   //also run r.js here
+  var redisClient = redis.createClient();
+
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  var sessionStore = new RedisStore({
+    client: redisClient
+  });
+  app.use(express.session({
+    store : sessionStore, 
+    secret: 'nooneknowsthis'
+  }));
+  app.use(express.methodOverride());
+
+  app.use(function(req, res, next){
+    res.locals.session = req.session;
+    next();
+  });
+
+  app.use(app.router);
   app.use(express.errorHandler()); 
 });
 
 // Routes
 require('./routes')(app);
 
-//app.listen(2727);
-//util.log("Express server listening on port " + app.address().port + " in mode " + app.settings.env);
-//logger.log("Express server listening on port " + app.address().port + " in mode " + app.settings.env);
 http.createServer(app).listen(config.port, function(){
-  logger.log("Express server listening on port " + config.port);
+  logger.log("Express server listening on port " + config.port + " in environment: " + app.settings.env);
 });
 
 module.exports = app; 
